c2b0c5963a2f3321ffa3f5d920a4493b053d2593
Gerris <gerrisprakash@gmail.com>
2025-07-08 17:28:13 +0530

    move key to env

--- src/lib/superbase.ts
+++ src/lib/superbase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://fescyshiqsvvnrnsrqwg.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZlc2N5c2hpcXN2dm5ybnNycXdnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE4ODY3MzAsImV4cCI6MjA2NzQ2MjczMH0.BZuUA8-vA6pyZkBfKmW_VfkQbphuV6olCa128x82tAo';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
